07ae822f0343d515eb78d34967e5944fa2a70bb4
mordva42 <333marina2007@gmail.com>
2019-06-28 13:15:26 -0700

    more resposible text and info section

---
+++ js/custom.js
@@ -0,0 +1,4 @@
+/************** Lozard lazy loader ****************/
+const el = document.querySelectorAll('img');
+const observer = lozad(el); // passing a `NodeList` (e.g. `document.querySelectorAll()`) is also valid
+observer.observe();
